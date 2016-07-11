e3ca101f0be72881ecdc2a77d92e4e4f4bb8b80c
wfdd <erdbanfed@gmail.com>
2016-07-11 22:35:28 +0300

    Use name__es for name

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,4 @@ require 'wikidata/fetcher'
 ids =
   EveryPolitician::Wikidata.wdq('claim[39:19254253]') |
   WikiData::Category.new('Categoría:Senadores de Colombia 2014-2018', 'es').wikidata_ids
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, names: {es: []})
